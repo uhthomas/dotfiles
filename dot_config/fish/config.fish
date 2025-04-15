2621f66db5f21c5f2bb2a4e88af6b85efac101d9
Thomas Way <thomas@6f.io>
2025-04-15 23:00:16 +0100

    update and cleanup

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    atuin init fish --disable-up-arrow | source
+    
+    abbr -a k kubectl
+    abbr -a ls eza
+    abbr -a open nautilus
+    abbr -a tree tree -a -I .git
+    abbr -a vi nvim
+    abbr -a vim nvim
+
+    bind \t complete-and-search
+end
---
+++ dot_config/fish/functions/kubectl.fish
@@ -0,0 +1,4 @@
+function kubectl --wraps=kubecolor --description 'alias kubectl=kubecolor'
+  kubecolor $argv
+        
+end
